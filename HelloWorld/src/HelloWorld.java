28bd9f8ef32ba2447038ce78fc188d3c2f81c73c
Anton <asntrx@gmail.com>
2016-02-21 19:31:14 +0300

    End of the intro

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ш on 21.02.16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hey, Joe!");
+        System.out.println("Hi, Jimi!");
+    }
+}
