9a3505f5efa5fcca9b42eaf2c86fa0888ff7e9ff
Anton <asntrx@gmail.com>
2016-03-08 17:38:36 +0300

    add dynamic

--- HelloWorld/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ш on 21.02.16.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hey, Joe!");
-        System.out.println("Hi, Jimi!");
-    }
-}
